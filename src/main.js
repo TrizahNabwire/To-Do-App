4899d53dfc0fbfcca625e432adc40ec176341ec1
TrizahNabwire <trizahnabwire20@gmail.com>
2024-01-14 01:06:49 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
